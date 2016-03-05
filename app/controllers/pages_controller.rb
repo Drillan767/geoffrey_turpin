b66c994ff8f9068076bc50cd8d09ca83f00289be
Joseph Levarato <Joseph Levarato>
2016-03-05 03:24:29 +0100

    Toutes pages crées, biographie manque JS, musiques manque indication mais moyen de créer un slide pour passer d'une partie à l'autre, binding des liens entre eux, création des liens pour mentions légales et plan du site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,24 @@
 class PagesController < ApplicationController
+
   def accueil
     @titre = "Accueil"
     render :layout => false
 
   end
+
   def musiques
     @titre = "Musiques"
   end
+
   def biographie
     @titre = "Biographie"
+  end
+
+  def mentions
+    @titre = "Mentions légales"
+  end
+
+  def mapping
+    @titre = "Plan du site"
   end
 end
