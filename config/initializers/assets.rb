e02ed1fc4772e0a79b8478e674c2fcbe93d06666
Joseph Levarato <Joseph Levarato>
2016-03-06 04:04:16 +0100

    Page "musiques" globalement terminée, il manque plus que le contenu et de modifier les vidéos selon ce que veut Geff.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( style.css )
+
+#CSS
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( scrollTo.js )
+Rails.application.config.assets.precompile += %w( script.js )
