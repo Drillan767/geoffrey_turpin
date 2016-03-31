6e057684d4da85dc510a501001d1a8a8ec5f5ccb
jd.levarato@gmail.com <jd.levarato@gmail.com>
2016-03-31 17:59:30 +0200

    correctif sur page musiques (js) et debut de boulot sur la frise biographie

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
-    //Script pour "biographie"
-	$('a.submenu').click(function () {
+    //Script pour "Musique"
+    $('.submenu').click(function () {
 
-		$('a.submenu').removeClass('selected');
-		$(this).addClass('selected');
-		
-		current = $(this);
-		
-		$('#wrapper').scrollTo($(this).attr('href'), 800);		
-		
-		return false;
+        var position = ($('.gauche').length>0) ? '100%' : '0%';
+        if ($('.gauche').length>0)
+            $('#wrapper').removeClass('gauche');
+        else
+            $('#wrapper').addClass('gauche');
+        $('#wrapper').scrollTo({top:'0%', left: position}, 800);
+
+        $('.submenu').removeClass("submenu-shown");
+        $(this).addClass("submenu-shown");
+
+    });
+
+	$(window).resize(function () {
+		resizePanel();
 	});
 
 	$(window).resize(function () {
@@ -17,18 +23,28 @@ $(document).ready(function() {
 	});
 
     //Script pour le header
+    //Définir sur quelle page on est
     var currentURL = document.location.href;
     $('div.header-link.active').removeClass('active');
     activeLinks = $('div.header-link a').filter(function () {
         return this.href === currentURL;
     }).closest('div.header-link').addClass('active');
 
+    //Effet de passage de la souris sur un lien
     $(".header-link").not(".active").mouseenter(function(){
         $(this).addClass("hover");
         $(this).parent().css("margin-top", "-3px")
     }).mouseleave(function(){
         $(this).removeClass("hover");
         $(this).parent().css("margin-top", "")
+    });
+
+    //Script pour "Musiques"
+
+    $("#b2009").on("click", function(){
+        $("p.selected").not(this).removeClass("selected");
+        $(this).addClass("selected");
+
     });
 
 	
