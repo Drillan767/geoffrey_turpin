b8a914e902f06b842c888092c561ddafaf991698
jd.levarato@gmail.com <jd.levarato@gmail.com>
2016-04-01 15:56:24 +0200

    Frise chronologique faite, reste un peu de CSS a fignoler, genre col xs

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+    $("#b2003").trigger("click");
+
     //Script pour "Musique"
     $('.submenu').click(function () {
 
@@ -39,15 +42,91 @@ $(document).ready(function() {
         $(this).parent().css("margin-top", "")
     });
 
-    //Script pour "Musiques"
+});
 
-    $("#b2009").on("click", function(){
-        $("p.selected").not(this).removeClass("selected");
-        $(this).addClass("selected");
+//Script pour "Musiques"
 
-    });
+//Partie Haute
+//Lorsque l'on clique sur "2009"
+$("#b2009").on("click", function(){
+    //Toutes les balises <p> ayant pour classe "selected" à part celle
+    // venant d'être cliquée perdent cette balise
+    // (Oui jQuery fait les choses VITES)
+    $("p.selected").not(this).removeClass("selected");
+    //Et toutes les balises <p> de classe "detceles" perdent leur balises également
+    $("p.detceles").removeClass("detceles");
+    //La cate venant d'être cliquée gagne une classe selected
+    $(this).addClass("selected");
+    //On vide le contenu du bloc "biography-desc (s'il y avait d'autres choses avant), et on rajoute :
+    $(".biography-desc").empty().append(
+        //Ceci :
+        "<h3>2009</h3>" +
+        "<p>Ceci est l'année 2009. C'était chouette, vraiment. Y'a eu une canicule, je crois.</p>"
+    );
 
-	
+});
+
+$("#b2015").on("click", function(){
+    $("p.selected").not(this).removeClass("selected");
+    $("p.detceles").removeClass("detceles");
+    $(this).addClass("selected");
+
+    $(".biography-desc").empty().append(
+        "<h3>2015</h3>" +
+        "<p>Et sinon ça va vous ? Moi je me fais un poil chier en vrai.</p>"
+    );
+
+});
+
+$("#soon").on("click", function(){
+    $("p.selected").not(this).removeClass("selected");
+    $("p.detceles").removeClass("detceles");
+    $(this).addClass("selected");
+
+    $(".biography-desc").empty().append(
+        "<h3>Bientôt</h3>" +
+        "<p>VOUS N'ETES PAS PREEEEEEEEETS !</p>"
+    );
+
+});
+
+//Partie Basse
+
+$("#b2003").on("click", function(){
+    $("p.selected").removeClass("selected");
+    $("p.detceles").not(this).removeClass("detceles");
+    $(this).addClass("detceles");
+
+    $(".biography-desc").empty().append(
+        "<h3>2003</h3>" +
+        "<p>Ceci est l'année 2003. C'était chouette, vraiment. Y'a eu une canicule, je crois.</p>"
+    );
+
+});
+
+
+$("#b2011").on("click", function(){
+    $("p.selected").removeClass("selected");
+    $("p.detceles").not(this).removeClass("detceles");
+    $(this).addClass("detceles");
+
+    $(".biography-desc").empty().append(
+        "<h3>2011</h3>" +
+        "<p>Cette année là j'ai passé mon bac. Je l'ai eu au rattrapage. MDR.</p>"
+    );
+
+});
+
+$("#b2016").on("click", function(){
+    $("p.selected").removeClass("selected");
+    $("p.detceles").not(this).removeClass("detceles");
+    $(this).addClass("detceles");
+
+    $(".biography-desc").empty().append(
+        "<h3>2016</h3>" +
+        "<p>Ceci est l'année 2009. C'était chouette, vraiment. Y'a eu une canicule, je crois.</p>"
+    );
+
 });
 
 function resizePanel() {
